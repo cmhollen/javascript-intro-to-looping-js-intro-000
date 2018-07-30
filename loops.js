2aeefdee29c8a91bd1c98a2f6fc6c4d737cab1f4
cmhollen <cmhollenkamp@gmail.com>
2018-07-30 19:10:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,13 @@
 function forLoop(arr){
   var arr = [];
+  var loop;
   for(var i = 0; i < 25; i++){
     if(i === 1){
-      arr += 'I am 1 strange loop.';
+      loop = 'I am 1 strange loop.';
     } else {
-      arr += `I am ${i} strange loops.`
+      loop = `I am ${i} strange loops.`
     }
-
+      arr += loop;
   }
   return arr;
 }
