680654121db22db55400578ce73ea88858f2f436
cmhollen <cmhollenkamp@gmail.com>
2018-07-30 20:01:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -13,13 +13,4 @@ function whileLoop(n){
   return 'done';
 }
 
-function maybeTrue() {
-  return Math.random() >= 0.5;
-}
-function doWhileLoop(array){
 
-
-  do {
-    array.pop()
-
-} while (maybeTrue());
